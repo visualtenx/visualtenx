ab4f377b1eb1a0974b3293d19e7c7008438d6586
visualtenx <visualtenx@gmail.com>
2024-08-04 06:55:22 -0700

    under development

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
-import { Button } from '@/components/ui/button'
 import React from 'react'
+import Navbar from '@/components/myui/Navbar'
 
-const Home = () => {
+
+const App = () => {
   return (
     <>
-    <div>Home</div>
-   <Button>Hello87678</Button>
+{/* 
+    <Navbar/>
+    <div>Home</div> */}
+     <div className='bg-red-600 border-spacing-2 h-screen flex justify-center items-center font-extrabold border-yellow-500 text-8xl text-white text-center '>
+      <span>Under development</span>
+     </div>
     </>
   )
 }
 
-export default Home
+export default App
---
+++ src/components/myui/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-red-500 flex justify-around'>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+    </div>
+  )
+}
+
+export default Navbar
